5d086ed505464194d690ddf6de82ee2a3e871524
JohnFelipe0 <159041468+JohnFelipe0@users.noreply.github.com>
2024-02-07 09:28:24 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function validaCampos(){
+
+    let usuario = document.getElementById("usuario").Value;
+    let senha = getElementById("senha").Value;
+
+    if(usuario != "juca" && senha != "12345")
+    alert("")
+}
